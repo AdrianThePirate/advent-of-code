d1577380dec8309b762bcb2546d90fa2775a1e84
Adrian Tveito Ingebrigtsen <adrian.techy@gmail.com>
2024-12-08 14:02:35 +0100

    Day 8 part 2 ✅

--- src/main/day8.go
+++ src/main/day8.go
@@ -15,6 +15,7 @@ func main() {
 	populateMap()
 	
 	fmt.Println("Result", len(findAnt()))
+	fmt.Println("Result", len(fintAntReson()))
 }
 
 func findAnt() map[adventutils.Vec2[int]]int {
@@ -29,12 +30,35 @@ func findAnt() map[adventutils.Vec2[int]]int {
 			}
 		}
 	}
+	return foundAnt
+}
 
+func fintAntReson() map[adventutils.Vec2[int]]int {
+	var foundAnt map[adventutils.Vec2[int]]int
+	foundAnt = make(map[adventutils.Vec2[int]]int)
+	for _, poss := range antPos {
+		for _, loc := range poss {
+			for _, pos := range poss {
+				if pos.Len(loc) == 0 { continue }
+				vector := pos.Sub(loc)
+				var i int = 1
+				for {
+					antPos := loc.Add(vector.Mul(i))
+					if antPos.X >= 0 && antPos.X < maxX && antPos.Y >= 0 && antPos.Y < maxY {
+						 foundAnt[antPos]++ 
+					} else {
+						break
+					}
+					i++
+				}
+			}
+		}
+	}
 	return foundAnt
 }
 
 func populateMap() {
-	file, err := os.Open("tasks/day8.txt")
+	file, err := os.Open("tasks/day8_sample.txt")
 	if err != nil {
 		fmt.Println(err)
 	}
