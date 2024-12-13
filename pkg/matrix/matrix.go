1df0b9218a6693a00e46c5615a62219f15819fbf
Adrian Tveito Ingebrigtsen <adrian.techy@gmail.com>
2024-12-13 21:52:47 +0100

    Updated Det to be more universal and recursive for NnN N > 2

--- pkg/matrix/matrix.go
+++ pkg/matrix/matrix.go
@@ -12,12 +12,37 @@ type Numeric interface {
 func Det[T Numeric](a array.Array2D[T]) (T, error){
 	y := len(a)
 	var emt T
-	if y < 2 { return emt, fmt.Errorf("not a valid matrix")}
+	if y < 1 { return emt, fmt.Errorf("not a valid matrix")}
 	for i := range a { if len(a[i]) != y { return emt, fmt.Errorf("not a XnX matrix") } }
 
-	switch y {
-	case 2:
+	switch {
+	case y == 1:
+		return a[0][0], nil
+	case y == 2:
 		return (a[0][0] * a[1][1]) - (a[0][1] * a[1][0]), nil
+	case y > 2:
+		var result T
+		for i := 0; i < y; i++{
+			var b array.Array2D[T]
+			for j := 1; j < y; j++{
+				var row array.Array[T]
+				for k := 0; k < y; k++{
+					if k == i { continue }
+					row = append(row, a[j][k])
+				}
+				b = append(b, row)
+			}
+			val, err := Det(b)
+			if err != nil { 
+				return emt, err
+			}
+			if i % 2 == 0{
+				result += (a[0][i] * val) 
+			} else {
+				result -= (a[0][i] * val)
+			}
+		}
+		return result, nil
 	default:
 		return emt, fmt.Errorf("not implemented support")
 	}
